b17e7511f52d6b34e548cd141003da13f5b93cfe
Kaylan <kaylanteodoro344@gmail.com>
2026-05-05 13:01:30 -0300

    auth controller iniciado

--- app/main.py
+++ app/main.py
@@ -2,8 +2,14 @@ from fastapi import FastAPI, Request, Depends
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, RedirectResponse
+from app.controllers import auth_controller
 
 app = FastAPI(title="Sistema de estoque")
 
 #Configurar o fastapi para serivir os arquivos estáticos (CSS, JS, Imagens)
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+templates = Jinja2Templates(directory="app/templates")
+
+#Incluir os routers dos controles
+app.include_router(auth_controller.router)
