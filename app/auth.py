996bc0a2a3ab1297d54378a1669776077252e5c5
Kaylan <kaylanteodoro344@gmail.com>
2026-05-18 12:55:00 -0300

    Adiciona controle de usuários com rotas administrativas e templates para gerenciamento

--- app/auth.py
+++ app/auth.py
@@ -74,3 +74,15 @@ def get_usuario_opcional(request: Request):
         return get_usuario_logado(request)
     except HTTPException:
         return None
+
+#Dependência do fastapi para administrador
+def get_admin(request: Request):
+    usuario = get_usuario_logado(request)
+
+    if usuario.get("role") != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Acesso apenas para administradores"
+        )
+    
+    return usuario
--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, RedirectResponse
 from app.controllers import auth_controller
+from app.controllers import admin_controller
 from app.auth import get_usuario_opcional
 
 app = FastAPI(title="Sistema de estoque")
@@ -14,6 +15,7 @@ templates = Jinja2Templates(directory="app/templates")
 
 #Incluir os routers dos controles
 app.include_router(auth_controller.router)
+app.include_router(admin_controller.router)
 
 #Tela inicial
 @app.get("/")
