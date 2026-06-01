c8f66dac70af5b9b0d9341846520cbab1d948729
Kaylan <kaylanteodoro344@gmail.com>
2026-06-01 10:53:23 -0300

    feat: create movimentacoes management interface and database schema
    - Added HTML templates for movimentacoes: form, historico, and index.
    - Implemented the index template to display a historical overview of movimentacoes with filtering options.
    - Created a new migration to establish the 'movimentacoes' table with necessary fields and constraints.
    - Added migrations for 'categorias' and 'produtos' tables to support the new movimentacoes functionality.

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ from app.controllers import auth_controller
 from app.controllers import admin_controller
 from app.controllers import categoria_controller
 from app.controllers import produto_controller
+from app.controllers import movimentacao_controller
 
 app = FastAPI(title="Sistema estoque")
 
@@ -21,6 +22,8 @@ app.include_router(auth_controller.router)
 app.include_router(admin_controller.router)
 app.include_router(categoria_controller.router)
 app.include_router(produto_controller.router)
+app.include_router(movimentacao_controller.router)
+
 #Tela inicial
 @app.get("/")
 def home(
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,9 @@
 from app.models import categoria
 from app.models import produto
 from app.models import usuario
+from app.models import movimentacao
+
+#Gerar a migration
+#python -m alembic revision --autogenerate -m "Criar tabela movimentacao"
+
+#python -m alembic upgrade head
